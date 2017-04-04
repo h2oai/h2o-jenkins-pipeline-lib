d4994e22ecb98adaec97abd71415830a8042beac
Nikhil <nikhilshekhar2000@gmail.com>
2017-04-04 15:39:33 -0700

    test script

--- vars/s3publish.groovy
+++ vars/s3publish.groovy
@@ -13,29 +13,74 @@ def call(String project, String files, String directoryOfBuild, String branchNam
     // This is really awful.  This old version of s3cmd does not set Content-Encoding metadata in S3.
     // The newer version of s3cmd sets the Content-Encoding to UTF-8 and gradle fails.
     // Alternately, we could strip off the Content-Encoding metadata tag for every file underneath maven.
-    def list_of_html_files
-    def list_of_js_files
-    def list_of_css_files
+
+    def list_of_files
     def f
- 
 
-    //sh  "s3cmd --rexclude='${directoryOfBuild}/maven' --acl-public sync ${directoryOfBuild}/ s3://h2o-release/h2o/${branchName}/${buildNumber}/"
-
-    //sh  "~jenkins/frozen/s3cmd-1.0.1/s3cmd --acl-public sync ${directoryOfBuild}/maven/ s3://h2o-release/h2o/${branchName}/${buildNumber}/maven/"
-    
-    echo "EXPLICITLY SET MIME TYPES AS NEEDED"
-
-    sh "list_of_html_files=`find dist/build -name '*.html' | sed 's/dist\\/build\\///g'`"
-    
-    echo "${list_of_html_files}"
-    
     sh """
-        for f in ${list_of_html_files}
+
+        s3cmd --rexclude='${directoryOfBuild}/maven' --acl-public sync ${directoryOfBuild}/ s3://h2o-release/h2o/${branchName}/${buildNumber}/
+
+        ~jenkins/frozen/s3cmd-1.0.1/s3cmd --acl-public sync ${directoryOfBuild}/maven/ s3://h2o-release/h2o/${branchName}/${buildNumber}/maven/
+
+        echo "EXPLICITLY SET MIME TYPES AS NEEDED"
+
+        list_of_files=`find ${directoryOfBuild} -name '*.html' | sed 's/${directoryOfBuild}\\///g'`
+
+        echo ${list_of_files}
+
+
+        for f in ${list_of_files}
         do
-            echo "Inside for"
             s3cmd --acl-public --mime-type text/html put ${directoryOfBuild}/${f} s3://h2o-release/h2o/${branchName}/${buildNumber}/${f}
         done
-    
+
+        list_of_files=`find ${directoryOfBuild} -name '*.js' | sed 's/${directoryOfBuild}\\///g'`
+        echo ${list_of_files}
+        for f in ${list_of_files}
+        do
+            s3cmd --acl-public --mime-type text/javascript put ${directoryOfBuild}/${f} s3://h2o-release/h2o/${branchName}/$buildNumber}/${f}
+        done
+
+        list_of_files=`find ${directoryOfBuild} -name '*.css' | sed 's/${directoryOfBuild}\\///g'`
+        echo ${list_of_files}
+        for f in ${list_of_files}
+        do
+            s3cmd --acl-public --mime-type text/css put ${directoryOfBuild}/${f} s3://h2o-release/h2o/${branchName}/${buildNumber}/${f}
+        done
+
+        echo "UPDATE LATEST POINTER"
+
+      #  tmpdir=./buildh2odev.tmp
+      #  mkdir -p ${tmpdir}
+      #  echo ${buildNumber} > ${tmpdir}/latest
+      #  echo "<head>" > ${tmpdir}/latest.html
+      #  echo "<meta http-equiv=\\"refresh\\" content=\\"0; url=${buildNumber}/index.html\\" />" >> ${tmpdir}/latest.html
+      #  echo "</head>" >> ${tmpdir}/latest.html
+
+      #  echo "PUSH TO S3"
+
+      #  s3cmd --acl-public put ${tmpdir}/latest s3://h2o-release/h2o/${branchName}/latest
+      #  s3cmd --acl-public put ${tmpdir}/latest.html s3://h2o-release/h2o/${branchName}/latest.html
+      #  s3cmd --acl-public put ${tmpdir}/latest.html s3://h2o-release/h2o/${branchName}/index.html
+
+      #  projectVersion =\$(cat ${directoryOfBuild}/project_version)
+
+      # if [ "$IS_LATEST_STABLE" = "true" ]; then
+      #     $THIS_DIR/h2o3-upload-stable-links.sh "$branchName" "$buildNumber" "$projectVersion"
+      # fi
+
+
+        # Update success pointer.
+       # cd ${branchdir}
+       # rm -f lastSuccessfulBuild
+       # ln -s ${buildNumber} lastSuccessfulBuild
     """
+
+
+    //branchdir , THIS_DIR, IS_LATEST_STABLE
+    //Which node to pick the artifacts from? or WHERE
+
+    ///Users/nikhilshekhar/pipeline/vars/s3publish.groovy
 
 }
