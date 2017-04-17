fb2effd420b00334e5c426bbbaa61921720b308c
Nikhil <nikhilshekhar2000@gmail.com>
2017-04-17 14:49:47 -0700

    Update s3publish_data_table.groovy

--- vars/s3publish_data_table.groovy
+++ vars/s3publish_data_table.groovy
@@ -15,9 +15,9 @@ def call(String project, String directoryOfMetaInfo, String directoryOfBuild, St
     //sh "s3cmd --acl-public sync ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ s3://ai.h2o.tests/intermittent_files/${branchName}/${buildNumber}/"
     
     def list_of_publishable_files = sh (
-        script: '''path=$(find ${env.directoryOfBuild}/build/lib.linux-x86_64-3.6/ -name '*.so')
+        script: """path=${(find ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ -name '*.so')}
                 name=$(basename "$path")
-                echo $name''',
+                echo $name""",
         returnStdout: true).split("\n")
     println list_of_publishable_files
     
