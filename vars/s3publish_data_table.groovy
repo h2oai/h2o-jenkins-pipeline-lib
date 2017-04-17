22f6d275cf9b57d9da75f628df5d19e5bf0232cf
Nikhil <nikhilshekhar2000@gmail.com>
2017-04-17 14:44:13 -0700

    Update s3publish_data_table.groovy

--- vars/s3publish_data_table.groovy
+++ vars/s3publish_data_table.groovy
@@ -1,8 +1,5 @@
 def call(String project, String directoryOfMetaInfo, String directoryOfBuild, String branchName, String buildNumber){
 
-    environment{
-        directoryOfBuild=directoryOfBuild
-    }
     echo "********Parameters received by this function call********"
     echo "Project to publish: ${project}"
     echo "Directory of Meta information to publish: ${directoryOfMetaInfo}"
@@ -15,9 +12,9 @@ def call(String project, String directoryOfMetaInfo, String directoryOfBuild, St
     //sh "s3cmd --acl-public sync ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ s3://ai.h2o.tests/intermittent_files/${branchName}/${buildNumber}/"
     
     def list_of_publishable_files = sh (
-        script: '''path=$(find ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ -name '*.so')
+        script: """path=$(find ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ -name '*.so')
                 name=$(basename "$path")
-                echo $name''',
+                echo $name""",
         returnStdout: true).split("\n")
     println list_of_publishable_files
     
