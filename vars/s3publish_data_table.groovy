31af6e86d7ac2eae63090c519626641ea8c10c13
Nikhil <nikhilshekhar2000@gmail.com>
2017-04-17 16:32:04 -0700

    Update s3publish_data_table.groovy

--- vars/s3publish_data_table.groovy
+++ vars/s3publish_data_table.groovy
@@ -9,7 +9,7 @@ def call(String project, String directoryOfMetaInfo, String directoryOfBuild, St
 
     //Publish the artifacts // > path | name=$(basename '$path' '.so') |echo "$name".so"
     //--rexclude='${directoryOfBuild}/build/lib.linux-x86_64-3.6/datatable/*'
-    sh "s3cmd --acl-public sync ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ --rexclude='${directoryOfBuild}/build/lib.linux-x86_64-3.6/datatable/*' s3://ai.h2o.tests/intermittent_files/${branchName}/${buildNumber}/"
+    sh "s3cmd --acl-public sync ${directoryOfBuild}/build/lib.linux-x86_64-3.6/*.so --rexclude='${directoryOfBuild}/build/lib.linux-x86_64-3.6/datatable/*' s3://ai.h2o.tests/intermittent_files/${branchName}/${buildNumber}/"
     
     def list_of_publishable_files = sh (
         script: "find ${directoryOfBuild}/build/lib.linux-x86_64-3.6/ -name '*.so'",
