f96e920323c14ec256b5abcd812bb36504d582b1
mmalohlava <michal@h2o.ai>
2017-06-23 15:24:38 -0700

    Debugging

--- vars/s3up.groovy
+++ vars/s3up.groovy
@@ -1,13 +1,16 @@
+#!/usr/bin/groovy
+
 /**
  * s3up localArtifact:String remoteArtifact:String keepPrivate:Boolean = true
  */
-def call(String localArtifact, String remoteArtifact, Boolean keepPrivate = true) {
-    /*def config = [:]
+def call(body) {
+    def config = [keepPrivate:true]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-    body()*/
+    body()
 
+    //echo s3cmd --access_key=${env.ACCESS_KEY} --secret_key=${env.SECRET_KEY} --acl-private put ${config.localArtifact} ${config.remoteArtifact} 
     sh '''
-    echo s3cmd --access_key=${env.ACCESS_KEY} --secret_key=${env.SECRET_KEY} --acl-private put ${localArtifact} ${remoteArtifact} 
+    echo ===============XXXXXXXX=
     '''
 }
