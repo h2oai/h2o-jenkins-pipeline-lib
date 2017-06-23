957a3dcd582c15632a3426ec94cf12217b93e88e
mmalohlava <michal@h2o.ai>
2017-06-23 16:40:02 -0700

    S3 credentials

--- vars/s3up.groovy
+++ vars/s3up.groovy
@@ -4,15 +4,17 @@
  * s3up localArtifact:String remoteArtifact:String keepPrivate:Boolean = true
  */
 def call(body) {
-    def config = [keepPrivate:true]
+    def config = [keepPrivate:true, credentialsId:"awsArtifactsUploader"]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
     def aclPrivate = config.keepPrivate ? "--acl-private" : ""
 
-    sh """
-    echo "Uploading artifacts: ${config}"
-    s3cmd --access_key=${env.ACCESS_KEY} --secret_key=${env.SECRET_KEY} ${aclPrivate} put ${config.localArtifact} ${config.remoteArtifact} 
-    """
+    withCredentials([[ $class: 'AmazonWebServicesCredentialsBinding', credentialsId: config.credentialsId]]) {
+        sh """
+        echo "Uploading artifacts: ${config}"
+        s3cmd --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_SECRET_ACCESS_KEY} ${aclPrivate} put ${config.localArtifact} ${config.remoteArtifact} 
+        """
+    }
 }
