de6df2559833413aa90832befb962fb756cd02e1
mmalohlava <michal@h2o.ai>
2017-06-30 15:55:45 -0700

    New environment functions

---
+++ src/ai/h2o/ci/Utils.groovy
@@ -0,0 +1,37 @@
+package ai.h2o.ci
+
+def getShell() {
+    new shell()
+}
+
+/**
+ * Return version suffix based on current branch
+ * and environment variable BUILD_ID.
+ */
+def getCiVersionSuffix() {
+    return "${env.BRANCH_NAME}_${env.BUILD_ID}"
+}
+
+def getCommmandOutput(String cmd) {
+    return getShell().pipe(cmd).trim()
+}
+
+def gitDescribeAll() {
+    return getShell().pipe("git describe --all --long HEAD").trim()
+}
+
+def gitDescribe() {
+    return getShell().pipe("git describe --always --long HEAD || echo none").trim()
+}
+
+def javaVersion() {
+    return getShell().pipe("java -version &2>1 || echo not found").trim()
+}
+
+def gitBranch() {
+    return getShell().pipe("git rev-parse --abbrev-ref HEAD").trim()
+}
+
+
+
+return this
--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -1,9 +1,23 @@
-def call() {
-    sh '''
-       echo "\n=== Environment ==="
+import ai.h2o.ci.Utils
+
+def call(String title = 'Environment') {
+    def utils = new Utils()
+
+    echo """
+    \033[1;33m
+    +===================+
+      ${title}
+    +===================+\033[0m
+    Git Describe : ${utils.gitDescribe()}
+    Git Describe : ${utils.gitDescribeAll()}
+    Git Branch   : ${utils.gitBranch()}
+    Java version : ${utils.javaVersion()}
+    """
+
+    /*sh """
        echo -e "\nJava version:\n$(java -version)"
        echo -e "\nEnvironment:\n$(env)"
-       '''
+       """*/
 }
 
 
