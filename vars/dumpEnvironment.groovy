4bb9781bf45af8213c9ebd00637929e092337a8f
mmalohlava <michal@h2o.ai>
2017-06-30 16:25:08 -0700

    Rollback

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -3,12 +3,10 @@ import ai.h2o.ci.Utils
 def utilsLib = new Utils()
 
 def call(String title = "Environment") {
-    ansiColor('xterm') {
-        sh """
-           printf "\n=== ${title} ===\n"
-           printf "\nJava version:\n$(java -version)"
-           printf "\nEnvironment:\n$(env)"
-           """
-    }
+    sh """
+       echo "\n=== ${title} ===\n"
+       echo "\nJava version:\n$(java -version)"
+       echo "\nEnvironment:\n$(env)"
+       """
 }
 
