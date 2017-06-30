ea2b2df32afef876da4b2272616ed58e8e24aa65
mmalohlava <michal@h2o.ai>
2017-06-30 16:44:57 -0700

    One more

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -1,6 +1,11 @@
 def call(String title = 'Environment') {
+    def s = """
+    echo ${title}
+    $(java -version)
+    """
     sh """
     echo -e "=== ${title} ==="
+    ${s}
     """
     /*sh """
        echo -e "\nJava version:\n$(java -version)"
