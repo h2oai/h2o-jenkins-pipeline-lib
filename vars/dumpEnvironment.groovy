9da7ad7779acc80e77b5c8020386a707f9ae8e6d
mmalohlava <michal@h2o.ai>
2017-06-30 16:23:38 -0700

    More changes

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -5,11 +5,10 @@ def utilsLib = new Utils()
 def call(String title = "Environment") {
     ansiColor('xterm') {
         sh """
-           printf "\n\001B[32m=== Environment ===\001B[0m\n"
+           printf "\n=== ${title} ===\n"
            printf "\nJava version:\n$(java -version)"
            printf "\nEnvironment:\n$(env)"
            """
     }
 }
-
 
