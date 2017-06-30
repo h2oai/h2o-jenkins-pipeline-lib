415252253419595c2debf06755eceab456fd9761
mmalohlava <michal@h2o.ai>
2017-06-30 16:01:15 -0700

    Fix printf

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -3,7 +3,7 @@ import ai.h2o.build
 def call(String title = "Environment") {
     ansiColor('xterm') {
         sh '''
-           printf "\n\e[32m=== ${title} ===\e[0m\n"
+           printf '\n\e[32m=== ${title} ===\e[0m\n'
            printf "\nJava version:\n$(java -version)"
            printf "\nEnvironment:\n$(env)"
            '''
