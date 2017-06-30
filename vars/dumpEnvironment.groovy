6cc51ae5682593c62f1fcfd3d52a4414b87a91e1
mmalohlava <michal@h2o.ai>
2017-06-30 16:29:06 -0700

    No clue

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -1,10 +1,10 @@
-import ai.h2o.ci.Utils
+//import ai.h2o.ci.Utils
 
-def utilsLib = new Utils()
+//def utilsLib = new Utils()
 
 def call(String title = "Environment") {
     sh """
-       echo -e "\n=== ===\n"
+       echo -e "\n=== ${title} ===\n"
        echo -e "\nJava version:\n$(java -version)"
        echo -e "\nEnvironment:\n$(env)"
        """
