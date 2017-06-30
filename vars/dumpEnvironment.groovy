24678c2c204daf1388825b40885c5d09a6ca107e
mmalohlava <michal@h2o.ai>
2017-06-30 16:21:20 -0700

    Fix of use of variable?

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -5,7 +5,7 @@ def utilsLib = new Utils()
 def call(String title = "Environment") {
     ansiColor('xterm') {
         sh """
-           printf "\n\001B[32m=== ${title} ===\001B[0m\n"
+           printf "\n\001B[32m=== Environment ===\001B[0m\n"
            printf "\nJava version:\n$(java -version)"
            printf "\nEnvironment:\n$(env)"
            """
