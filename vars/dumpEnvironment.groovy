ef30be39bf41c001f3da35a88e4050308da4edf8
mmalohlava <michal@h2o.ai>
2017-06-30 17:05:01 -0700

    Env color

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -4,12 +4,13 @@ def call(String title = 'Environment') {
     def utils = new Utils()
 
     echo """
+    "\033[1;33m
     +===================+
       ${title}
     +===================+
     Git Describe: ${utils.gitDescribeAll()} 
     Java version: ${utils.javaVersion()}
-
+    \033[0m $1"
     """
 
     /*sh """
