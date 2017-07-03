6d771cb10781665ba2b4d903cb7f6373331e7076
mmalohlava <michal@h2o.ai>
2017-07-02 23:31:14 -0700

    Color experiment

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -1,28 +1,26 @@
-@Grab('de.vandermeer:asciitable:0.3.2')
+//@Grab('de.vandermeer:asciitable:0.3.2')
 import ai.h2o.ci.Utils
-import de.vandermeer.asciitable.AsciiTable
+import static ai.h2o.ci.ColorUtils.*
+//import de.vandermeer.asciitable.AsciiTable
 
 def call(String title = 'Environment') {
     def utils = new Utils()
-    def at = new AsciiTable()
+    /*def at = new AsciiTable()
     at.addRule()
     at.addRow("Git Describe", utils.gitDescribe())
     at.addRule()
-    def t = at.render()
+    def t = at.render()*/
     echo "$t"
 
-
     echo """
-    \033[1;33m
     +===================+
-      ${title}
-    +===================+\033[0m
+      ${green(${title})}
+    +===================+
     Git Describe : ${utils.gitDescribe()}
     Git Describe : ${utils.gitDescribeAll()}
     Git Branch   : ${utils.gitBranch()}
     Java version : ${utils.javaVersion()}
     """
-    at = null
 }
 
 
