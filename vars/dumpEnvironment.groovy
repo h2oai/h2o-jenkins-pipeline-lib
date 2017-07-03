a1c94dd42ba31dfd81aa9c94e82d7722e1031de8
mmalohlava <michal@h2o.ai>
2017-07-02 23:17:26 -0700

    Table

--- vars/dumpEnvironment.groovy
+++ vars/dumpEnvironment.groovy
@@ -1,7 +1,16 @@
+@Grab('de.vandermeer:asciitable:0.3.2')
 import ai.h2o.ci.Utils
+import de.vandermeer.asciitable.AsciiTable
 
 def call(String title = 'Environment') {
     def utils = new Utils()
+    def at = new AsciiTable()
+    at.addRule()
+    at.addRow("Git Describe", utils.gitDescribe())
+    at.addRule()
+    def t = at.render()
+    echo "$t"
+
 
     echo """
     \033[1;33m
@@ -13,11 +22,7 @@ def call(String title = 'Environment') {
     Git Branch   : ${utils.gitBranch()}
     Java version : ${utils.javaVersion()}
     """
-
-    /*sh """
-       echo -e "\nJava version:\n$(java -version)"
-       echo -e "\nEnvironment:\n$(env)"
-       """*/
+    at = null
 }
 
 
