3c9ded96b2e12cbc2349e67404ccae89e816bf46
mmalohlava <michal@h2o.ai>
2017-07-03 12:09:11 -0700

    Exp

--- src/ai/h2o/ci/TableUtils.groovy
+++ src/ai/h2o/ci/TableUtils.groovy
@@ -2,7 +2,7 @@ package ai.h2o.ci
 
 @Grab('de.vandermeer:asciitable:0.3.2')
 import de.vandermeer.asciitable.AsciiTable
-import de.vandermeer.asciitable.CWC_LongestWordMin
+import de.vandermeer.asciitable.*
 
 @NonCPS
 def table2cols(data, colSizes = [:], int leftMargin = 4, int maxWidth = 80) {
@@ -17,7 +17,8 @@ def table2cols(data, colSizes = [:], int leftMargin = 4, int maxWidth = 80) {
     // Generate constraints
     int[] colWidths = [-1, -1] as int[]
     //colSizes.each {int idx, int width -> colWidths[idx] = width}
-    def cwc = new CWC_LongestWordMin(colWidths)
+    //def cwc = new CWC_LongestWordMin(colWidths)
+    def cwc = new CWC_LongestWordMax(colWidths)
     at.getRenderer().setCWC(cwc)
     at.render(maxWidth)
 }
