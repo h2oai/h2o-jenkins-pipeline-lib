b9fbd028d568f3b28362cd68c32945ab3224dd0e
mmalohlava <michal@h2o.ai>
2017-07-17 17:24:09 -0700

    Accept multiple parameters

--- vars/savePyBuildInfo.groovy
+++ vars/savePyBuildInfo.groovy
@@ -3,8 +3,10 @@ import ai.h2o.ci.Utils
 /**
  * Generate Python build information into a file given file.
  */
-def call(String targetFile) {
+def call(String ...targetFile) {
     def utils = new Utils()
     def content = utils.getPyBuildInfo()
-    writeFile file: targetFile, text: content
+    for(String tf : targetFile) {
+        writeFile file: targetFile, text: content
+    }
 }
