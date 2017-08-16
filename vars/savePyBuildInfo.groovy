699d93608da8e97798961f1076e024dbcfed27e3
mmalohlava <michal@h2o.ai>
2017-08-16 15:42:43 -0700

    generated version file with baseVersion

--- vars/savePyBuildInfo.groovy
+++ vars/savePyBuildInfo.groovy
@@ -3,10 +3,14 @@ import ai.h2o.ci.Utils
 /**
  * Generate Python build information into a file given file.
  */
-def call(boolean isRelease, String ...targetFile) {
+def call(boolean isRelease, String baseVersion, String ...targetFile) {
     def utils = new Utils()
-    def content = utils.getPyBuildInfo(isRelease)
+    def content = utils.getPyBuildInfo(isRelease, baseVersion)
     for(int i = 0; i < targetFile.length; i++) {
         writeFile file: targetFile[i], text: content
     }
+}
+
+def call(boolean isRelease, String ...targetFile) {
+    call(isRelease, null, targetFile)
 }
