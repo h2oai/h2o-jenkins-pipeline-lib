5c4103ce1e9847945e7e8d2a01a1467e86917b6b
Anmol Bal <anmol@h2oai.com>
2017-09-06 15:45:16 -0700

    WIP
    need to add filtering of PR jobs

---
+++ vars/cancelBuild.groovy
@@ -0,0 +1,22 @@
+import ai.h2o.ci.BuildInfo
+
+def abortPreviousRunningBuilds() {
+  def hi = Hudson.instance
+  def pname = env.JOB_NAME.split('/')[0]
+
+  hi.getItem(pname).getItem(env.JOB_BASE_NAME).getBuilds().each{ build ->
+    def exec = build.getExecutor()
+
+    if (build.number != currentBuild.number && exec != null) {
+      exec.interrupt(
+        Result.ABORTED,
+        new CauseOfInterruption.UserInterruption(
+          "Aborted by #${currentBuild.number}"
+        )
+      )
+      println("Aborted previous running build #${build.number}")
+    } else {
+      println("Build is not running or is current build, not aborting - #${build.number}")
+    }
+  }
+}
