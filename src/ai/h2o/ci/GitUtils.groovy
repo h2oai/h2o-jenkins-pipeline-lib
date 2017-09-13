c7e00a8be09e33e1fc567139b3350239991f9838
nkalonia1 <nkalonia1@gmail.com>
2017-09-13 15:22:01 -0700

    Exposed GitUtils as a standalone variable (#4)

--- src/ai/h2o/ci/GitUtils.groovy
+++ src/ai/h2o/ci/GitUtils.groovy
@@ -8,6 +8,10 @@ class GitUtils implements Serializable {
         this.script = script
     }
 
+    public static GitUtils create(script) {
+	return new GitUitls(script)
+    }
+
     def pipe(command) {
         return this.script.sh(script: command, returnStdout: true)
     }
