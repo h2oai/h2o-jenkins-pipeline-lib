ce2c52287e1450fa12724d90e34dbebd0604158a
mmalohlava <michal@h2o.ai>
2017-09-15 16:39:06 -0700

    Fix typo

--- vars/copyArch.groovy
+++ vars/copyArch.groovy
@@ -1,6 +1,6 @@
 def call(String projectName, String buildId, String fileFilter) {
     step([$class: 'CopyArtifact',
-        projectName: projectName
+        projectName: projectName,
         filter: fileFilter,
         selector: [$class: 'SpecificBuildSelector', buildNumber: buildId]
         ])
