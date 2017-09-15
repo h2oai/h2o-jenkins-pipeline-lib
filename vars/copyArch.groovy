2406f1a2cb1a914fba9aaf8ba0eee7aae2dcd26a
mmalohlava <michal@h2o.ai>
2017-09-15 16:55:42 -0700

    Rename test reports

--- vars/copyArch.groovy
+++ vars/copyArch.groovy
@@ -4,5 +4,11 @@ def call(String projectName, String buildId, String fileFilter) {
         filter: fileFilter,
         selector: [$class: 'SpecificBuildSelector', buildNumber: buildId]
         ])
+    files = findFiles(fileFilter)
+    setJUnitPrefix(projectName, files)
 }
 
+def setJUnitPrefix(prefix, files) {
+  // add prefix to qualified classname
+  sh "shopt -s globstar && sed -i \"s/\\(<testcase .*classname=['\\\"]\\)\\([a-z]\\)/\\1${prefix.toUpperCase()}.\\2/g\" $files"
+}
