4e5b1da18c55bd7672aef29ea2a1c17bb0387e63
mmalohlava <michal@h2o.ai>
2017-09-15 16:36:48 -0700

    CopyArch utility

---
+++ vars/copyArch.groovy
@@ -0,0 +1,8 @@
+def call(String projectName, String buildId, String fileFilter) {
+    step([$class: 'CopyArtifact',
+        projectName: projectName
+        filter: fileFilter,
+        selector: [$class: 'SpecificBuildSelector', buildNumber: buildId]
+        ])
+}
+
