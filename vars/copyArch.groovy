02b474f4d32b842eaebca452a1ab4e4a7f1d099e
mmalohlava <michal@h2o.ai>
2017-09-15 22:00:20 -0700

    Remove "/" from strings

--- vars/copyArch.groovy
+++ vars/copyArch.groovy
@@ -6,13 +6,13 @@ def call(String projectName, String buildId, String fileFilter) {
         ])
     // THis needs a Pipeline utility plugin: https://github.com/jenkinsci/pipeline-utility-steps-plugin
     files = findFiles(glob: fileFilter)*.path.join(" ")
-    setJUnitPrefix(projectName, files)
+    setJUnitPrefix(projectName.toUpperCase().replaceAll("/",""), files)
 }
 
 def setJUnitPrefix(prefix, files) {
   // add prefix to qualified classname
   //sh "shopt -s globstar && sed -i \"s/\\(<testcase .*classname=['\\\"]\\)\\([a-z]\\)/\\1${prefix.toUpperCase()}.\\2/g\" $files"
   sh """
-     sed -i -e "s/\\(<testcase classname=['\\\"]\\)\\(.\\)/\\1${prefix.toUpperCase()}.\\2/g" $files
+     sed -i -e "s/\\(<testcase classname=['\\\"]\\)\\(.\\)/\\1${prefix}.\\2/g" $files
      """
 }
