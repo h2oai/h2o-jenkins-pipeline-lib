20784cdadac261ff55730e135210a36390321e97
Michal Raška <michalr@h2o.ai>
2017-10-23 19:10:48 +0200

    [ITA-64] cancelBuild helper (#8)
    * [ITA-64] Rename method to call
    
    * [ITA-64] Modify for regular and multibranch pipelines as well
    
    * Rename method to reflect what it really does

---
+++ vars/cancelPreviousBuilds.groovy
@@ -0,0 +1,31 @@
+def call() {
+  def hi = Hudson.instance
+  def jobNameParts = env.JOB_NAME.tokenize('/')
+  def pname = jobNameParts[0]
+
+  def item = hi.getItem(pname)
+  // if called from multibranch pipeline/folder etc., we need to get the builds of the leaf item
+  if (jobNameParts.size() > 1) {
+    // remove the first element, because that has been already used to get the root item
+    jobNameParts.remove(0)
+    for (part in jobNameParts) {
+      item = item.getItem(part)
+    }
+  }
+
+  item.getBuilds().each{ build ->
+    def exec = build.getExecutor()
+
+    if (build.number != currentBuild.number && exec != null) {
+      exec.interrupt(
+        Result.ABORTED,
+        new CauseOfInterruption.UserInterruption(
+          "Aborted by #${currentBuild.number}"
+        )
+      )
+      println("Aborted previous running build #${build.number}")
+    } else {
+      println("Build is not running or is current build, not aborting - #${build.number}")
+    }
+  }
+}
