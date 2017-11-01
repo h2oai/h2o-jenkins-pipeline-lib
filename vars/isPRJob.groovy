cdac286d71630192f56153b83a015263f3f2426f
Michal Raška <michal.raska@gmail.com>
2017-11-01 11:44:11 +0100

    [ITA-63] Moved helpers to BuildInfo class

--- vars/isPRJob.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-  // Only PR builds have the CHANGE_BRANCH set
-  return env.CHANGE_BRANCH != null && env.CHANGE_BRANCH != ''
-}
--- vars/startedByTimer.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-  // may fail if the sandbox permissions are missing
-  return currentBuild.rawBuild.getAction(CauseAction.class).findCause(hudson.triggers.TimerTrigger.TimerTriggerCause.class) != null
-}
