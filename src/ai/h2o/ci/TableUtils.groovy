7894548bd07461b0263bf91498a6489a080b9042
mmalohlava <michal@h2o.ai>
2018-01-31 22:39:50 -0800

    Table decorator

--- src/ai/h2o/ci/TableUtils.groovy
+++ src/ai/h2o/ci/TableUtils.groovy
@@ -3,11 +3,12 @@ package ai.h2o.ci
 import com.cloudbees.groovy.cps.NonCPS
 
 @NonCPS
-def table2cols(data, colSizes = [:], int leftMargin = 4, int maxWidth = 80) {
+def table2cols(data, colSizes = [:], decorators = [:], int leftMargin = 4, int maxWidth = 80) {
     def out = new StringBuffer()
+    def nop = { e -> e }
     data.each { k, v ->
-        out << k.padRight(colSizes.getOrDefault(0, 20))
-        out << v
+        out << decorators.getOrDefault(0, nop)(k.padRight(colSizes.getOrDefault(0, 20)))
+        out << decorators.getOrDefault(1, nop)(v)
         out << '\n'
     }
     return out.toString()
