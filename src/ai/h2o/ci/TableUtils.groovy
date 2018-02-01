225f6d474f6c2b2443da2cc18fdc08e2fb8556cb
mmalohlava <michal@h2o.ai>
2018-01-31 22:29:32 -0800

    Remove grab of library

--- src/ai/h2o/ci/TableUtils.groovy
+++ src/ai/h2o/ci/TableUtils.groovy
@@ -1,26 +1,16 @@
 package ai.h2o.ci
 
 import com.cloudbees.groovy.cps.NonCPS
-@Grab('de.vandermeer:asciitable:0.3.2')
-import de.vandermeer.asciitable.AsciiTable
-import de.vandermeer.asciitable.*
 
 @NonCPS
 def table2cols(data, colSizes = [:], int leftMargin = 4, int maxWidth = 80) {
-    def utils = new Utils()
-    def at = new AsciiTable()
-    at.addRule()
+    def out = new StringBuffer()
     data.each { k, v ->
-      at.addRow(k, v)
+        out << k.padRight(20)
+        out << v
+        out << '\n'
     }
-    at.addRule()
-    at.getContext().setFrameLeftMargin(leftMargin)
-    // Generate constraints
-    int[] colWidths = [-1, -1] as int[]
-    colSizes.each {int idx, int width -> colWidths[idx] = width}
-    def cwc = new CWC_LongestWordMin(colWidths)
-    at.getRenderer().setCWC(cwc)
-    at.render(maxWidth)
+    return out.toString()
 }
 
 return this
