e22303d3fa45133631c04f4c8cd0fab6a882016b
mmalohlava <michal@h2o.ai>
2018-03-14 13:32:16 -0700

    Fix typo

--- vars/err.groovy
+++ vars/err.groovy
@@ -1,11 +1,12 @@
 import static ai.h2o.ci.Utils.ex2str
 import static ai.h2o.ci.ColorUtils.*
 
+def call(Exception e) {
+    call(ex2str(e))
+}
+
 def call(String msg) {
     echo "[ERROR] !!! ${red(msg)} !!!"
     error msg
 }
 
-def call(Exception e) {
-    call(ex2str(e))
-}
