9f9c4949d1912ce6d2a7946dfbb631ef91f4f492
Michal Raška <michalr@h2o.ai>
2018-10-23 12:49:21 +0200

    :fire: Fix retryWithTimeout.

--- vars/retryWithTimeout.groovy
+++ vars/retryWithTimeout.groovy
@@ -1,7 +1,26 @@
 def call(int timeoutSeconds, int retries, block) {
-    retry(retries) {
-        timeout(time: timeoutSeconds, unit: 'SECONDS') {
-            block()
+    // DOES NOT WORK BECAUSE OF BUG IN JENKINS.
+    // https://issues.jenkins-ci.org/browse/JENKINS-51454
+    //
+    // retry(retries) {
+    //     timeout(time: timeoutSeconds, unit: 'SECONDS') {
+    //         block()
+    //     }
+    // }
+    
+    for (def i = 0; i < retries; i++) {
+        try {
+            timeout(time: timeoutSeconds, unit: 'SECONDS') {
+                block()
+                break
+            }
+        } catch (Exception e) {
+            if (i == (retries - 1)) {
+                throw e
+            }
+            def sw = new StringWriter()
+            e.printStackTrace(new PrintWriter(sw))
+            echo sw.toString()
         }
     }
 }
