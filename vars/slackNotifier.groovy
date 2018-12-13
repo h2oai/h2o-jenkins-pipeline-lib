042259c1912dcc5f1318ec54b8bf0d32423e256a
Anmol Bal <anmol@h2oai.com>
2018-12-13 11:47:22 -0800

    create a slack notifying lib

---
+++ vars/slackNotifier.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} failed"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} is unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} the result was unclear"	
+  }
+}
