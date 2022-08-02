51b1df3a25e7c2fd5f1e7347dcebbbaf411240cc
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2022-08-02 09:49:53 +0200

    Update isJenkinsJob.groovy

--- vars/isJenkinsJob.groovy
+++ vars/isJenkinsJob.groovy
@@ -3,5 +3,6 @@ import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
 
 def call(def jobName, def branchName) {
     def job = Jenkins.get().getItem(jobName)
-    print(job)
+    job = job as org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+    print job.getBranch()
 }
