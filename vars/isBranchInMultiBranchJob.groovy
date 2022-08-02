7b3e73bd9b8db4e5a847a78e63c95d76afced5d8
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2022-08-02 10:16:21 +0200

    Update isBranchInMultiBranchJob.groovy
    fix formatting for groovy

--- vars/isBranchInMultiBranchJob.groovy
+++ vars/isBranchInMultiBranchJob.groovy
@@ -3,16 +3,16 @@ import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
 
 def call(def multiBranchJobName, def branchName) {
     def job = Jenkins.get().getItem(multiBranchJobName)
-    if(job){
+    if (job) {
         try {
             job = job as WorkflowMultiBranchProject
             def branchFound = job.getBranch(branchName) as boolean
-            if(branchFound){
+            if (branchFound) {
                 info("Branch ${branchName} exists for job ${multiBranchJobName}")
                 return true
             }
             return false
-        } catch (org.codehaus.groovy.runtime.typehandling.GroovyCastException ex){
+        } catch (org.codehaus.groovy.runtime.typehandling.GroovyCastException ex) {
             error("The job is not MultibranchJob " + ex)
         }
     }
