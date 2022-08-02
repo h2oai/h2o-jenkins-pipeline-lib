f1b51a58c92e68a152f49c2bdcf89ce1ec3be7bd
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2022-08-02 11:33:41 +0200

    check if a branch exists for a Multibranch job (#52)
    * Create isJenkinsJob.groovy
    
    check if job exists
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update and rename isJenkinsJob.groovy to .groovy
    
    * Rename .groovy to isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Update isJenkinsJob.groovy
    
    * Rename isJenkinsJob.groovy to isBranchInMultiBranchJob.groovy
    
    * Update isBranchInMultiBranchJob.groovy
    
    fix formatting for groovy

---
+++ vars/isBranchInMultiBranchJob.groovy
@@ -0,0 +1,20 @@
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+
+def call(def multiBranchJobName, def branchName) {
+    def job = Jenkins.get().getItem(multiBranchJobName)
+    if (job) {
+        try {
+            job = job as WorkflowMultiBranchProject
+            def branchFound = job.getBranch(branchName) as boolean
+            if (branchFound) {
+                info("Branch ${branchName} exists for job ${multiBranchJobName}")
+                return true
+            }
+            return false
+        } catch (org.codehaus.groovy.runtime.typehandling.GroovyCastException ex) {
+            error("The job is not MultibranchJob " + ex)
+        }
+    }
+    return false
+}
