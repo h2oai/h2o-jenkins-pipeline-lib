27f0c84edee326bcb07bef58a44a56048857327a
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2022-08-02 09:40:05 +0200

    Update isJenkinsJob.groovy

--- vars/isJenkinsJob.groovy
+++ vars/isJenkinsJob.groovy
@@ -1,3 +1,10 @@
-def call(def jobName) {
-    return jenkins.model.Jenkins.instance.getItem(jobName) != null
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+
+def call(def jobName, def branchName) {
+    def job = Jenkins.get().getItem(jobName)
+    if (job instanceof WorkflowMultiBranchProject){
+       def exists = (WorkflowMultiBranchProject) job.getItemByBranchName(branchName)
+       return boolean(exist)
+    }
 }
